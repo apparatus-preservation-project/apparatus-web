d6cda59cb265a5ee5c1d14591134d45b910a9de2
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-05-28 21:55:00 +0200

    New improved dark mode that doesn't suck

--- tools/compile-scss-sassc.sh
+++ tools/compile-scss-sassc.sh
@@ -2,7 +2,9 @@
 
 common_arguments="--style compressed --load-path ./"
 
+sassc ${common_arguments} assets/scss/darkmode.scss assets/css/darkmode.css
 sassc ${common_arguments} assets/scss/style.scss assets/css/style.css
 
 # Compress compiled stylesheets for nginx gzip_static
+gzip assets/css/darkmode.css
 gzip assets/css/style.css
